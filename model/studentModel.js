0fc32b0a640a9db85efc979ac276bfc535be0805
doquockhanh <dqkhanh293@gmail.com>
2024-10-28 10:29:34 +0700

    update

--- model/studentModel.js
+++ model/studentModel.js
@@ -2,10 +2,11 @@ import mongoose from "mongoose";
 
 const studentSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  studentCode: { type: String, required: true, unique: true },
-  isActive: { type: Boolean, default: true }
+  studentCode: { type: String, required: true },
+  isActive: { type: Boolean, default: true },
 });
 
-const studentModel = mongoose.models.student || mongoose.model("students", studentSchema)
+const studentModel =
+  mongoose.models.student || mongoose.model("students", studentSchema);
 
 export default studentModel;
